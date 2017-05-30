b393e91611bfea24f38c66c2065b5ec0b006c2c7
乔岩 <qiaoyan@hujiang.com>
2017-05-30 18:35:39 +0800

    remove unuseful file

---
+++ QYCategorys/Classes/QYCategorys.h
@@ -0,0 +1,15 @@
+//
+//  QYCategorys.h
+//  Pods
+//
+//  Created by 乔岩 on 2017/5/26.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+#import "UIColor+HexString.h"
+
+@interface QYCategorys : NSObject
+
+@end
