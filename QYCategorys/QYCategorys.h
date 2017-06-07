40a2371290f21ff4bdb6f86ea03613dfce9ae74c
乔岩 <qiaoyan@hujiang.com>
2017-06-07 17:34:47 +0800

    test

--- QYCategorys/QYCategorys.h
+++ QYCategorys/QYCategorys.h
@@ -7,19 +7,20 @@
 //
 
 #ifndef QYCategorys_h
-#define QYCategorys_h
-#if __has_include(<QYCategorys/QYCategorys.h>)
-FOUNDATION_EXPORT double QYCategorysVersionNumber;
-FOUNDATION_EXPORT const unsigned char QYCategorysVersionString[];
-#import <QYCategorys/UIColor+QYHexString.h>
-#import <QYCategorys/NSString+QYVerify.h>
-#import <QYCategorys/NSString+QYSize.h>
-#import <QYCategorys/UIView+QYPositioning.h>
-#else
-#import "UIColor+QYHexString.h"
-#import "NSString+QYVerify.h"
-#import "NSString+QYSize.h"
-#import "UIView+QYPositioning.h"
-#endif
+    #define QYCategorys_h
+
+    #if __has_include(<QYCategorys/QYCategorys.h>)
+        FOUNDATION_EXPORT double QYCategorysVersionNumber;
+        FOUNDATION_EXPORT const unsigned char QYCategorysVersionString[];
+        #import <QYCategorys/UIColor+QYHexString.h>
+        #import <QYCategorys/NSString+QYVerify.h>
+        #import <QYCategorys/NSString+QYSize.h>
+        #import <QYCategorys/UIView+QYPositioning.h>
+    #else
+        #import "UIColor+QYHexString.h"
+        #import "NSString+QYVerify.h"
+        #import "NSString+QYSize.h"
+        #import "UIView+QYPositioning.h"
+    #endif
 #endif
 
